4812988f9068a109bf2621ae20d8d774972abd72
bipvadone <hebe-er-tt@hotmail.com>
2021-10-24 07:58:51 -0500

    Se agregan puntos en los textos

--- back-end/VA-Iknelia/backend/awsLex/coreLex.js
+++ back-end/VA-Iknelia/backend/awsLex/coreLex.js
@@ -13,12 +13,12 @@ module.exports.dialog=  function(intentRequest, callback){
     if (slots.slotAmount !== null && intentRequest.inputTranscript.includes(slots.slotAmount)) {
         return exeDinamoDB.getAmount(userId).then(item=>{
             if(parseInt(item.Item.tddAmount)>=parseInt(slots.slotAmount)){
-                mensaje=`Se le hara una trasnferencia a ${slots.slotName} por la cantidad de ${slots.slotAmount}. ¿Deseas continuar?`
+                mensaje=`Se le hara una trasnferencia a ${slots.slotName} por la cantidad de ${slots.slotAmount} pesos. ¿Deseas continuar?`
                 return callback(responseLex.confirmIntent(sessionAttributes,intentRequest.currentIntent.name,slots,mensaje));
             }
             else{
                 slots.slotAmount=null;
-                mensaje="No cuentas con saldo suficiente, elige otro monto"
+                mensaje="No cuentas con saldo suficiente. elige otro monto"
                 return callback(responseLex.elicitSlot(sessionAttributes, intentName, slots, 'slotAmount',mensaje));
             } 
         })
@@ -39,10 +39,10 @@ module.exports.dialog=  function(intentRequest, callback){
         let opcion= arrayOpcion[arrayOpcion.length-1]-1; 
         return exeDinamoDB.getContacts(userId, slots.slotName).then(item=>{
             if(!item[opcion]){
-                mensaje="La opcion "+(opcion+1)+ " es incorrecta, selecciona una opcion valida";
+                mensaje="La opcion "+(opcion+1)+ " es incorrecta. selecciona una opcion valida";
                 return callback(responseLex.elicitSlot(sessionAttributes, intentName, slots, 'slotChooseName',mensaje));
             }
-            mensaje="Tu contacto "+item[opcion].name+" con terminacion de cuenta "+ item[opcion].cuenta.substring(15,19)+", ¿Es correcto?";
+            mensaje="Tu contacto "+item[opcion].name+" con terminacion de cuenta "+ item[opcion].cuenta.substring(15,19)+". ¿Es correcto?";
             return callback(responseLex.elicitSlot(sessionAttributes, intentName, slots, 'slotCorrectContact',mensaje));
         })
     }
@@ -50,12 +50,12 @@ module.exports.dialog=  function(intentRequest, callback){
     if (slots.slotName !== null && intentRequest.inputTranscript.includes(slots.slotName)) {
         return exeDinamoDB.getContacts(userId, slots.slotName).then(item=>{
             if(!item){
-                mensaje="Al parecer no tienes el contacto llamado "+slots.slotName+". Por favor, dime el nombre de nuevo";
+                mensaje="Al parecer no tienes el contacto llamado "+slots.slotName+". Por favor. dime el nombre de nuevo";
                 slots.slotName=null;
                 return callback(responseLex.elicitSlot(sessionAttributes, intentName, slots, 'slotName',mensaje));
              }
             if(item.length==1){
-                mensaje="Tu contacto "+item[0].name+" con terminacion de cuenta "+ item[0].cuenta.substring(15,19)+", ¿Es correcto?";
+                mensaje="Tu contacto "+item[0].name+" con terminacion de cuenta "+ item[0].cuenta.substring(15,19)+". ¿Es correcto?";
                 return callback(responseLex.elicitSlot(sessionAttributes, intentName, slots, 'slotCorrectContact',mensaje));
             }
             if(item.length>1){
@@ -63,7 +63,7 @@ module.exports.dialog=  function(intentRequest, callback){
                 let mensaje = '';
                 for(let i=0; i < item.length;i++)
                 {
-                    opciones +=`, opcion ${i + 1}, terminacion de cuenta ${item[i].cuenta.substring(15,19)}`;
+                    opciones +=`. opcion ${i + 1}. terminacion de cuenta ${item[i].cuenta.substring(15,19)}`;
                 }
                 mensaje=`Tienes ${item.length} contactos, que opcion eliges: ${opciones}`;
                 return callback(responseLex.elicitSlot(sessionAttributes, intentName, slots, 'slotChooseName',mensaje));
