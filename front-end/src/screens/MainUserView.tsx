4a1e7f8f2f04e1e4fde5d17c641542ba19775f7f
Saul Rodriguez Garcia <saulrosg@hotmail.com>
2021-10-23 19:14:58 -0500

    se agrega funcionalidad de voz

--- front-end/src/screens/MainUserView.tsx
+++ front-end/src/screens/MainUserView.tsx
@@ -2,7 +2,8 @@ import React, { useEffect, useState } from 'react'
 import { View, Text, Image, TouchableOpacity, Button} from 'react-native';
 import { StackScreenProps } from '@react-navigation/stack';
 import { mainUserTheme } from '../themes/mainUserTheme';
-
+import Voice, { SpeechRecognizedEvent, SpeechResultsEvent, SpeechErrorEvent } from '@react-native-voice/voice';
+import Tts from 'react-native-tts';
 
 interface Props extends StackScreenProps<any, any> {};
 
@@ -18,7 +19,25 @@ export const MainUserView = ({ navigation}: Props, propUser:userProps) => {
     const goToSuccesfulT = (() => {
         navigation.navigate('TransferSuccessful');
     })
- 
+    const startRecording = async () => {
+        try {
+            await Voice.start('es-MX');
+
+        } catch (e) {
+            console.error("Error" + e);
+        }
+    }
+    const silenceTimer = setTimeout(() => {
+        Voice.stop();
+    }, 1000)
+    Voice.onSpeechResults = (e: SpeechResultsEvent) => {
+        clearTimeout(silenceTimer);
+    }
+    const startSpeech = ((texto: string) => {
+        Tts.stop();
+        Tts.speak(texto);
+        console.log("hablando")
+    })
     return (
         <View style={mainUserTheme.container}>
             <View style={mainUserTheme.imageContainer}>
@@ -43,6 +62,12 @@ export const MainUserView = ({ navigation}: Props, propUser:userProps) => {
                 </View>
             </View>
             <View style={mainUserTheme.micContainer}>
+                <TouchableOpacity onPress={startRecording}>
+                    <Image
+                        source={require('../assets/images/mic.gif')}
+                        style={mainUserTheme.micImage}
+                    />
+                </TouchableOpacity>
             </View>
         </View>
     )
