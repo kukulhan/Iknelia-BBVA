3df90637a6aab2af28fa62bf98be18283e995602
Saul Rodriguez Garcia <saulrosg@hotmail.com>
2021-10-24 04:46:54 -0500

    se agrega comentario

--- front-end/src/api/lexAPI.tsx
+++ front-end/src/api/lexAPI.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const awsLexAPI= () => axios.create({
-    baseURL: 'https://slnlcpkaga.execute-api.us-west-2.amazonaws.com',
+    baseURL: 'https://8fcz26f8u6.execute-api.us-west-2.amazonaws.com',
     headers:{'Content-Type':'application/json'}
 })
 
