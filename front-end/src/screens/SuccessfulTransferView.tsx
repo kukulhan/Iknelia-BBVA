7e66d1ff1c12dd449e872be0f3cd0916211b763d
Saul Rodriguez Garcia <saulrosg@hotmail.com>
2021-10-23 22:50:18 -0500

    se agrega api transactionView

--- front-end/src/screens/SuccessfulTransferView.tsx
+++ front-end/src/screens/SuccessfulTransferView.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { View, Text, Image, TouchableOpacity } from 'react-native';
 import { SuccessfulTransferTheme } from '../themes/successfulTransferTheme';
+import awsIkneliaAPI from '../api/ikneliaAPI';
 import { useState, useEffect, useRef } from 'react';
 import Icon from 'react-native-vector-icons/Ionicons';
 import { StackScreenProps } from '@react-navigation/stack';
@@ -21,6 +22,44 @@ export const SuccessfulTransferView = ({ navigation }: Props) => {
         transactionId: string;
     }
 
+    const [stateTransfer, setStateTransfer] = useState<Transfer>({
+        date:'',
+        amount:'',
+        dateSamll:'',
+        concept:'',
+        accountType:'',
+        contact:'',
+        account:'',
+        transactionId:''
+    });
+
+    const executeSuccessfulTransfer = async () => {
+        await awsIkneliaAPI().get<Transfer>('/Prod/getlasttransaction/12345678').then(responseTransfer => {
+            if (responseTransfer.status === 200) {
+                setStateTransfer({
+                    date:responseTransfer.data.date,
+                    amount: responseTransfer.data.amount,
+                    dateSamll: responseTransfer.data.dateSamll,
+                    concept: responseTransfer.data.concept,
+                    accountType: responseTransfer.data.accountType,
+                    contact: responseTransfer.data.contact,
+                    account: responseTransfer.data.account,
+                    transactionId: responseTransfer.data.transactionId
+                })
+            }
+        })
+    }
+
+    const isMountedT = useRef(true);
+
+    useEffect(() => {
+        if (!isMountedT.current) return;
+        executeSuccessfulTransfer();
+        return () => {
+            isMountedT.current = false;
+        }
+    }, [])
+
     const goToHome = (() => {
         navigation.navigate('Assistant');
     })
@@ -28,14 +67,14 @@ export const SuccessfulTransferView = ({ navigation }: Props) => {
     return (
         <View style={SuccessfulTransferTheme.container}>
             <View style={SuccessfulTransferTheme.charge} >
-                <Text style={SuccessfulTransferTheme.mainText}>000</Text>
+                <Text style={SuccessfulTransferTheme.mainText}>Cargo a tu cuenta *{stateTransfer.account.substring(stateTransfer.account.length-4, stateTransfer.account.length)} de {stateTransfer.amount}.00 </Text>
             </View>
             <View style={SuccessfulTransferTheme.successfulTransfer}>
                 <Text style={SuccessfulTransferTheme.mainTextSuccess}>Transferencia exitosa</Text>
-                <Text style={SuccessfulTransferTheme.subText}>000</Text>
+                <Text style={SuccessfulTransferTheme.subText}>{stateTransfer.date}</Text>
                 <Text style={SuccessfulTransferTheme.amount}>
                     <Text style={SuccessfulTransferTheme.textUp}>$</Text>
-                    <Text>000</Text>
+                    <Text>{stateTransfer.amount}</Text>
                     <Text style={SuccessfulTransferTheme.textUp}>.00</Text>
                 </Text>
                 <Text style={SuccessfulTransferTheme.subText}> Esta transferencia no genera comisión</Text>
@@ -47,17 +86,17 @@ export const SuccessfulTransferView = ({ navigation }: Props) => {
             <View style={SuccessfulTransferTheme.personeOne}>
                 <Image style={SuccessfulTransferTheme.avatar} source={require('../assets/images/heberillo.jpg')} />
                 <Text style={SuccessfulTransferTheme.accountTitle}>Cuenta</Text>
-                <Text style={SuccessfulTransferTheme.accountInf}>00000</Text>
+                <Text style={SuccessfulTransferTheme.accountInf}>*{stateTransfer.account.substring(stateTransfer.account.length-4, stateTransfer.account.length)}</Text>
             </View>
             <View style={SuccessfulTransferTheme.personeTwo}></View>
             <View style={SuccessfulTransferTheme.personeTwoView}>
-                <Text style={SuccessfulTransferTheme.personeTwoText}> 000 </Text>
-                <Text style={SuccessfulTransferTheme.accountTitle}>0000</Text>
-                <Text style={SuccessfulTransferTheme.accountInf}>00000</Text>
+                <Text style={SuccessfulTransferTheme.personeTwoText}> {stateTransfer.contact.substring(0,2).toLocaleUpperCase()} </Text>
+                <Text style={SuccessfulTransferTheme.accountTitle}>{stateTransfer.contact.charAt(0).toUpperCase()+stateTransfer.contact.slice(1) }</Text>
+                <Text style={SuccessfulTransferTheme.accountInf}>{(stateTransfer.accountType === 'tdd')? 'Tarjeta de Débito ': 'Tarjeta de Crédito'}</Text>
             </View>
             <View style={SuccessfulTransferTheme.concept} >
                 <Text style={SuccessfulTransferTheme.accountInf}>Concepto</Text>
-                <Text style={SuccessfulTransferTheme.conceptInf}>0000</Text>
+                <Text style={SuccessfulTransferTheme.conceptInf}>{stateTransfer.concept}</Text>
             </View>
             <TouchableOpacity  style={SuccessfulTransferTheme.backButtom} onPress={goToHome}>
                 <Icon
